2d8a72581d6de561c0efdbf1f1e98033448f5c96
Kong Xiangyu <kongxi16@cdf.toronto.edu>
2016-10-03 18:31:12 -0400

    unfinished ripplecarry

--- Lab3/partI/mux7to1.v
+++ Lab3/partI/mux7to1.v
@@ -1,59 +1,21 @@
-module mux2to1(x, y, s, m);
-	input x, y, s;
-	output m;
-	
-	assign m = (~s & x) | (s & y);
-endmodule
-
 module mux7to1(SW, LEDR);
 	input [9:0] SW;
 	output [1:0]LEDR;
+	reg m;
 	
-	wire connector_0;
-	wire connector_1;
-	wire connector_2;
-	wire connector_3;
-	wire connector_4;
+	always @(*)
+	begin
+		case(SW[9:7])
+			3'b000: m = SW[0];
+			3'b001: m = SW[1];
+			3'b010: m = SW[2];
+			3'b011: m = SW[3];
+			3'b100: m = SW[4];
+			3'b101: m = SW[5];
+			3'b110: m = SW[6];
+			default: m = SW[7];
+		endcase
+	end
+	assign LEDR[0]= m;
 	
-	mux2to1 m0(
-		.x(SW[0]),
-		.y(SW[1]),
-		.s(SW[7]),
-		.m(connector_0)
-	);
-	
-	mux2to1 m1(
-		.x(SW[2]),
-		.y(SW[3]),
-		.s(SW[7]),
-		.m(connector_1)
-	);
-	
-	mux2to1 m2(
-		.x(SW[4]),
-		.y(SW[5]),
-		.s(SW[7]),
-		.m(connector_2)
-	);
-	
-	mux2to1 m3(
-		.x(connector_0),
-		.y(connector_1),
-		.s(SW[8]),
-		.m(connector_3)
-	);
-	
-	mux2to1 m4(
-		.x(SW[6]),
-		.y(connector_2),
-		.s(SW[8]),
-		.m(connector_4)
-	);
-	
-	mux2to1 m5(
-		.x(connector_3),
-		.y(connector_4),
-		.s(SW[9]),
-		.m(LEDR[0])
-	);
 endmodule
--- Lab3/partII/ripplecarry.v
+++ Lab3/partII/ripplecarry.v
@@ -1,9 +1,49 @@
-module fulladder(a, b, c_in, c_out, s);
+module fulladder(a, b, cin, cout, s);
 	input a;
 	input b;
-	input c_in;
-	output c_out;
+	input cin;
+	output cout;
+	output s;
+	reg cout
 	
-	assign s = c_in ^ a;
-	assign c_out = 
+	assign s = cin ^ a;
+	always @(*)
+	begin
+		case[a ^ b]
+			1'b0: cout = b;
+			1'b1: cout = cin;
+		endcase
+	end
+endmodule
+
+
+module ripplecarry(SW, LEDR);
+	input [9:0] SW;
+	output [4:0] LEDR;
+	
+	wire c0, c1, c2, c3, c4, c5;
+	
+	fulladder fa0(
+		.a(SW[1]),
+		.b(SW[5]),
+		.cin(SW[0]),
+		.cout(c0),
+		.s(LEDR[0])
+	);
+	
+	fulladder fa1(
+		.a(SW[2]),
+		.b(SW[5]),
+		.cin(c0),
+		.cout(c1),
+		.s(LEDR[1])
+	);
+	
+	fulladder fa2(
+		.a(SW[3]),
+		.b(SW[6]),
+		.cin(c1),
+		.cout(c2),
+		.s(LEDR[2])
+	);
 endmodule
