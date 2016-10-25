d530f08afb0da9154b6b5c035a647a67faa42378
Kong Xiangyu <kongxi16@cdf.toronto.edu>
2016-10-25 18:54:46 -0400

    finished part III

--- Lab5/partIII/morseencoder.do
+++ Lab5/partIII/morseencoder.do
@@ -1,9 +1,21 @@
 vlib work
 
-vlog -timescale 1ns/1ps clkrcounter.v
+vlog -timescale 1ns/1ns morseencoder.v
 
-vsim clkrcounter
+vsim morseencoder
 
 log {/*}
 
 add wave {/*}
+
+force {rate} 0;
+
+force {key[2: 0]} 2#110
+
+force {start} 1 0, 0 20
+
+force {clk} 0 0, 1 10 -r 20
+
+force {asr_n} 1 0, 0 1, 1 2
+
+run 1800ns
--- tryingshitout/try.v
+++ tryingshitout/try.v
@@ -1,4 +1,27 @@
-module try(out);
-	output [25:0] out;
-	assign out = 26'd49999999;
+module try(enable, load, par_load, asr_n, clk, out);
+	input enable, par_load, asr_n, clk;
+	input [9:0] load;
+	output reg out;
+	
+	reg [9:0] q;
+	
+	always @(posedge clk, negedge asr_n)
+	begin
+		if (asr_n == 0)
+			begin
+			out <= 0;
+			q <= 14'b0;
+			end
+		else if (par_load == 1)
+			begin
+			out <= 0;
+			q <= load;
+			end
+		else if (enable == 1)
+			begin
+			out <= q[0];
+			q <= q >> 1'b1;
+			end
+	end
+
 endmodule
