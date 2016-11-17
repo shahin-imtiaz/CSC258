30d4f2be0d38fbcafc77aef6b6ab95033e089c10
Xiangyu Kong <danielkong1997@gmail.com>
2016-11-16 21:03:50 -0500

    lab1

--- Project/airplaneLu/airplane.v
+++ Project/airplaneLu/airplane.v
@@ -1,3 +1,88 @@
+module airplane
+	(
+		CLOCK_50,						//	On Board 50 MHz
+		// Your inputs and outputs here
+        KEY,
+        SW,
+		// The ports below are for the VGA output.  Do not change.
+		VGA_CLK,   						//	VGA Clock
+		VGA_HS,							//	VGA H_SYNC
+		VGA_VS,							//	VGA V_SYNC
+		VGA_BLANK_N,						//	VGA BLANK
+		VGA_SYNC_N,						//	VGA SYNC
+		VGA_R,   						//	VGA Red[9:0]
+		VGA_G,	 						//	VGA Green[9:0]
+		VGA_B   						//	VGA Blue[9:0]
+	);
+
+	input			CLOCK_50;				//	50 MHz
+	input   [9:0]   SW;
+	input   [3:0]   KEY;
+
+	// Declare your inputs and outputs here
+	// Do not change the following outputs
+	output			VGA_CLK;   				//	VGA Clock      
+	output			VGA_HS;					//	VGA H_SYNC
+	output			VGA_VS;					//	VGA V_SYNC
+	output			VGA_BLANK_N;				//	VGA BLANK
+	output			VGA_SYNC_N;				//	VGA SYNC
+	output	[9:0]	VGA_R;   				//	VGA Red[9:0]
+	output	[9:0]	VGA_G;	 				//	VGA Green[9:0]
+	output	[9:0]	VGA_B;   				//	VGA Blue[9:0]
+	
+	
+	wire resetn;
+	assign resetn = KEY[0];
+	
+	// Create the colour, x, y and writeEn wires that are inputs to the controller.
+	wire [2:0] colour;
+	wire [7:0] x;
+	wire [7:0] y;
+	wire writeEn;
+	wire enable,ld_c;
+
+	// Create an Instance of a VGA controller - there can be only one!
+	// Define the number of colours as well as the initial background
+	// image file (.MIF) for the controller.
+	vga_adapter VGA(
+			.resetn(resetn),
+			.clock(CLOCK_50),
+			.colour(colour),
+			.x(x),
+			.y(y),
+			.plot(writeEn),
+			/* Signals for the DAC to drive the monitor. */
+			.VGA_R(VGA_R),
+			.VGA_G(VGA_G),
+			.VGA_B(VGA_B),
+			.VGA_HS(VGA_HS),
+			.VGA_VS(VGA_VS),
+			.VGA_BLANK(VGA_BLANK_N),
+			.VGA_SYNC(VGA_SYNC_N),
+			.VGA_CLK(VGA_CLK));
+		defparam VGA.RESOLUTION = "160x120";
+		defparam VGA.MONOCHROME = "FALSE";
+		
+		
+		defparam VGA.BITS_PER_COLOUR_CHANNEL = 1;
+		defparam VGA.BACKGROUND_IMAGE = "black.mif";
+			
+	// Put your code here. Your code should produce signals x,y,colour and writeEn/plot
+	// for the VGA controller, in addition to any other functionality your design may require.
+    
+    // Instansiate datapath
+	// datapath d0(...);
+      datapath d0(enable,KEY[3],KEY[2],CLOCK_50,ld_c,KEY[0],x,y,colour);
+
+    // Instansiate FSM control
+    // control c0(...);
+	   control c0(CLOCK_50,KEY[0],~KEY[1],enable,ld_c,writeEn);
+
+    
+endmodule
+
+
+
 module counter(clock,reset_n,enable,q);
 	input clock,reset_n,enable;
 	output reg [1:0] q;
@@ -43,16 +128,16 @@ module delay_counter(clock,reset_n,enable,q);
 		input clock;
 		input reset_n;
 		input enable;
-		output reg [3:0] q;
+		output reg [19:0] q;
 		
 		always @(posedge clock)
 		begin
 			if(reset_n == 1'b0)
-				q <= 4'b1111;
+				q <= 20'd0;
 			else if(enable ==1'b1)
 			begin
-			   if ( q == 4'b0000 )
-					q <= 4'b1111;
+			   if ( q == 20'd0 )
+					q <= 20'd1666666;
 				else
 					q <= q - 1'b1;
 			end
@@ -70,7 +155,7 @@ module frame_counter(clock,reset_n,enable,q);
 			q <= 4'b0000;
 		else if(enable == 1'b1)
 		begin
-		  if(q == 4'b1111)
+		  if(q == 4'b1010)
 			  q <= 4'b0000;
 		  else
 			  q <= q + 1'b1;
@@ -97,14 +182,14 @@ module y_counter(clock,reset_n,enable,signal_1,signal_2,q);
 				if(q == 8'd0)
 			    q <= q ;
 			   else
-				 q <= q + 1'b1;
+				 q <= q - 1'b1;
 		   end
 			else if((signal_1 == 0) & (signal_2 == 1))
 			begin
-			   if(q == 8'd119)
+			   if(q == 8'd110)
 				 q <= q;
 				else
-				 q <= q - 1'b1;
+				 q <= q + 1'b1;
 			end
 			else if((signal_1 == 1) & (signal_2 == 1))
 			    q <= q;
@@ -159,23 +244,23 @@ module datapath(enable,signal_1,signal_2,clock,ld_c,reset_n,X,Y,colour_out);
 	output[7:0] X,Y;
 	output[2:0] colour_out;
 	
-	wire[3:0] c0;
+	wire[19:0] c0;
 	wire[3:0] c1;
 	wire[7:0] y_in;
 	wire[2:0] colour_1;
 	
 	delay_counter m1(clock,reset_n,enable,c0);
-	assign enable_1 = (c0 ==  4'b0000) ? 1 : 0;
+	assign enable_1 = (c0 ==  20'd0) ? 1 : 0;
 	frame_counter m2(clock,reset_n,enable_1,c1);
-	assign enable_2 = (c1 == 4'b1111) ? 1 : 0;
+	assign enable_2 = (c1 == 4'b1010) ? 1 : 0;
 	y_counter m4(enable_2,reset_n,enable,signal_1,signal_2,y_in);
-	assign colour_1 = (c1 == 4'b1111) ? 3'b000 : 3'b101;
+	assign colour_1 = (c1 == 4'b1010) ? 3'b000 : 3'b101;
 	datapath1 m7(8'd10,y_in,colour_1,ld_c,clock,reset_n,enable,X,Y,colour_out);
 endmodule
 
-module control(clock,reset_n,go,enable,ld_c);
+module control(clock,reset_n,go,enable,ld_c,plot);
 	input clock,reset_n,go;
-	output reg enable,ld_c;	
+	output reg enable,ld_c,plot;	
 	
 	reg [3:0] current_state, next_state;
 	
@@ -198,11 +283,13 @@ module control(clock,reset_n,go,enable,ld_c);
         // By default make all our signals 0
         ld_c = 1'b0;
 		  enable = 1'b0;
+		  plot = 1'b0;
 		  
 		  case(current_state)
 				S_MOVE:begin
 				   ld_c = 1'b1;
 					enable = 1'b1;
+					plot = 1'b1;
 					end
 		  endcase
     end
@@ -215,3 +302,24 @@ module control(clock,reset_n,go,enable,ld_c);
             current_state <= next_state;
       end 
 endmodule
+
+
+	
+	
+	
+	
+
+
+
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
