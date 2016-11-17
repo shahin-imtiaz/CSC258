dccc4432b8c4587a12fa2472a31f7225aa0bfcd5
Kong Xiangyu <kongxi16@cdf.toronto.edu>
2016-11-17 17:15:21 -0500

    unfinished airplane

--- Project/airplane.v
+++ Project/airplane.v
@@ -1,34 +1,136 @@
-//module airplane_top (
-//		CLOCK50,
-//		VGA_CLK,   						//	VGA Clock
-//		VGA_HS,							//	VGA H_SYNC
-//		VGA_VS,							//	VGA V_SYNC
-//		VGA_BLANK_N,						//	VGA BLANK
-//		VGA_SYNC_N,						//	VGA SYNC
-//		VGA_R,   						//	VGA Red[9:0]
-//		VGA_G,	 						//	VGA Green[9:0]
-//		VGA_B   						//	VGA Blue[9:0])
-//
-//		);
-//		
-//endmodule
-//
-//
-//
-//
-//
-//module airplane(clk, reset_n, enable, up, down);
-//	input CLOCK50;
-//	input [] KEY;
-//	
-//	datapath d(clk, reset_n, enable, ld_x, ld_y, ld_color)
-//	
-//endmodule
-//
-//
+module airplane
+	(
+		CLOCK_50,						//	On Board 50 MHz
+      // Use KEY and SW for now and connect to keyboard latter
+		KEY,
+      SW,
+		
+		VGA_CLK,   						//	VGA Clock
+		VGA_HS,							//	VGA H_SYNC
+		VGA_VS,							//	VGA V_SYNC
+		VGA_BLANK_N,						//	VGA BLANK
+		VGA_SYNC_N,						//	VGA SYNC
+		VGA_R,   						//	VGA Red[9:0]
+		VGA_G,	 						//	VGA Green[9:0]
+		VGA_B   						//	VGA Blue[9:0]
+	);
+
+	input	CLOCK_50;				//	50 MHz
+	input [9:0] SW;
+	input [3:0] KEY;
+
+	// Declare your inputs and outputs here
+	// Do not change the following outputs
+	output VGA_CLK;   				//	VGA Clock      
+	output VGA_HS;					//	VGA H_SYNC
+	output VGA_VS;					//	VGA V_SYNC
+	output VGA_BLANK_N;				//	VGA BLANK
+	output VGA_SYNC_N;				//	VGA SYNC
+	output [9:0] VGA_R;   				//	VGA Red[9:0]
+	output [9:0] VGA_G;	 				//	VGA Green[9:0]
+	output [9:0]	VGA_B;   				//	VGA Blue[9:0]
+	
+	
+	wire resetn;
+	assign resetn = KEY[0];
+	wire up;
+	assign up = KEY[3];
+	wire down = KEY[2];
+	
+	
+	// Create the colour, x, y and writeEn wires that are inputs to the controller.
+	wire [2:0] colour_out;
+	wire [8:0] x_out;
+	wire [7:0] y_out;
+	wire writeEn;
+	wire enable, ld_color;
+
+	// Create an Instance of a VGA controller - there can be only one!
+	// Define the number of colours as well as the initial background
+	// image file (.MIF) for the controller.
+	vga_adapter VGA(
+			.resetn(resetn),
+			.clock(CLOCK_50),
+			.colour(colour_out),
+			.x(x_out),
+			.y(y_out),
+			.plot(writeEn),
+			/* Signals for the DAC to drive the monitor. */
+			.VGA_R(VGA_R),
+			.VGA_G(VGA_G),
+			.VGA_B(VGA_B),
+			.VGA_HS(VGA_HS),
+			.VGA_VS(VGA_VS),
+			.VGA_BLANK(VGA_BLANK_N),
+			.VGA_SYNC(VGA_SYNC_N),
+			.VGA_CLK(VGA_CLK));
+		defparam VGA.RESOLUTION = "160x120";
+		defparam VGA.MONOCHROME = "FALSE";
+		
+		
+		defparam VGA.BITS_PER_COLOUR_CHANNEL = 1;
+		defparam VGA.BACKGROUND_IMAGE = "black.mif";
+			
+	// Put your code here. Your code should produce signals x,y,colour and writeEn/plot
+	// for the VGA controller, in addition to any other functionality your design may require.
+    
+   // Instansiate datapath
+	datapath d(CLOCK_50, resetn, enable, draw, color_in, ld_x, ld_y, ld_color, up, down, x_out, y_out, color_out)
+//   datapath d0(enable,KEY[3],KEY[2],CLOCK_50,ld_c,KEY[0],x,y,colour);
+
+   // Instansiate FSM control
+	control c(CLOCK_50, resetn, enable, ld_color, writeEn);
+//	control c0(CLOCK_50,KEY[0],~KEY[1],enable,ld_c,writeEn);
+
+    
+endmodule
 
 
-module datapath(clk, reset_n, enable, draw, color_in, ld_x, ld_y, ld_color, up, down x_out, y_out, color_out);
+
+
+module control(clk, reset_n, enable, ld_color, writeEn);
+	input clk, reset_n;
+	output reg enable, ld_color, writeEn;
+
+	reg [1:0] current_state, next_state;
+
+	localparam 	LOAD = 2'b00, LOAD_WAIT = 2'b01, DRAW = 2'b10;
+
+	always @(*) begin
+		case (current_state)
+		LOAD: next_state = (!reset_n) ? LOAD_WAIT : LOAD;
+		LOAD_WAIT: next_state = (!reset_n) ? LOAD_WAIT : DRAW;
+		DRAW: next_state = DRAW;
+		endcase
+	end
+
+	always @(*) begin
+		enable = 1'b0;
+		ld_color = 1'b0;
+		writeEn = 1'b0;
+		case (current_state)
+			DRAW: begin
+				enable = 1'b1;
+				ld_color = 1'b1;
+				writeEn = 1'b1;
+			end
+		endcase
+	end
+
+	always @(posedge clk) begin
+		if (!reset_n) begin
+			current_state <= LOAD;
+		end
+		else begin
+			current_state <= next_state;
+		end
+	end
+endmodule
+
+
+
+
+module datapath(clk, reset_n, enable, draw, color_in, ld_x, ld_y, ld_color, up, down, x_out, y_out, color_out);
 	input clk, reset_n, enable, draw;
 	input ld_x, ld_y, ld_color;
 	input up, down;
@@ -48,6 +150,13 @@ module datapath(clk, reset_n, enable, draw, color_in, ld_x, ld_y, ld_color, up, 
 	// where y starts
 	reg [7:0] y_start;
 	
+	// when rate division done, frame enabled
+	wire [20:0] rate_out;
+	wire frame_enable;
+	// when frame division done, x enabled 
+	wire [3:0] frame_out;
+	wire x_enable;
+	// used for enabling y to count when x is done
 	wire y_enable;
 	
 	// register for x, y_start, color
@@ -72,6 +181,15 @@ module datapath(clk, reset_n, enable, draw, color_in, ld_x, ld_y, ld_color, up, 
 			end
 		end
 	end
+	
+	
+	// rate divider
+	rate_divider rate(clk, reset_n, enable, rate_out); 
+	assign frame_enable = (rate_out == 20'd0) 1 : 0;
+	
+	// frame counter
+	frame_counter frame(clk, frame_enable, reset_n, frame_out);
+	assign x_enable = (frame_out == 4'd10) 1 : 0;
 	
 	// x counter for square 4 * 4 plane
 	// threshold = 2'b11
@@ -187,48 +305,22 @@ module frame_counter(clk, enable, reset_n, out);
 endmodule
 
 
-
-module control(clk, reset_n, go, enable, ld_color, write_enable);
-	input clk, reset_n, go;
-	output enable, ld_color;
-
-	reg [1:0] current_state, next_state;
-
-	localparam 	LOAD = 2'b00,
-				LOAD_WAIT = 2'b01;
-				DRAW = 2'b10;
-
-	always @(*) begin
-		case (current_state)
-		LOAD: next_state = go ? LOAD_WAIT : LOAD;
-		LOAD_WAIT: next_state = go ? LOAD_WAIT : DRAW;
-		DRAW: next_state = DRAW;
-	end
-
-	always @(*) begin
-		enable = 1'b0;
-		ld_color = 1'b0;
-		case (current_state)
-			DRAW: begin
-				enable = 1'b1;
-				ld_color = 1'b1;
+// rate divider that divides the 
+module rate_divider(clk, reset_n, enable, out);
+		input clk;
+		input reset_n;
+		input enable;
+		output reg [19:0] out;
+		
+		always @(posedge clk)
+		begin
+			if (!reset_n)
+				out <= 20'd0;
+			else if (enable) begin
+			   if (out == 20'd0)
+					out <= 20'd1666666;
+				else
+					out <= out - 1'b1;
 			end
-		endcase
-	end
-
-	always @(posedge clk) begin
-		if (!reset_n) begin
-			current_state <= LOAD;
 		end
-		else begin
-			current_state <= next_state;
-		end
-	end
 endmodule
-
-
-
-
-
-
-
